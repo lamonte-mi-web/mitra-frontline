9b0fdb23a4f43fef9728c32d511b546455b08fd3
lamonte-mi-web <datatimlamonte@gmail.com>
2025-08-09 10:28:14 +0700

    Redo

--- src/app/(home)/Sections/HeroSection.tsx
+++ src/app/(home)/Sections/HeroSection.tsx
@@ -14,19 +14,12 @@ export default function HeroSection() {
             />
             <div className="absolute inset-0 bg-black/30 z-10" />
 
-            <div className="absolute inset-0 z-20 flex flex-col items-center justify-center text-center text-white p-4">
-                <h2 className="text-[3.5rem] font-bold mb-3 max-w-2xl">
-                    Bayangkan Baju Impian, Kami Jahitkan Tanpa Batas
+            <div className="absolute inset-0 z-20 flex flex-col items-center justify-center text-center p-4">
+                <h2 className="text-[3rem] font-bold text-white mb-3 max-w-2xl">
+                    99% Mitra Sukses Menjadi Mitra Kami
                 </h2>
-                <p className="text-[1.5rem]">
-                    Rasakan kain lembut membungkus tubuh Anda. Dari seragam kebanggaan tim
-                    hingga gaun yang memikat setiap tatapan. Semua mudah, langsung dari pabrik
-                    kami untuk Anda.
-                </p>
                 <OrangeDivider className="mb-3" />
-                <CustomButton href="https://wa.me/628111829921?text=Halo%20Kak%20Erni%2C%20saya%20tertarik%20menjadi%20Mitra%20Lamonte.%20Bisa%20dibantu%20penjelasannya%3F">
-                    Mulai Konsultasi Gratis
-                </CustomButton>
+                <CustomButton href="https://wa.me/628111829921?text=Halo%20Kak%20Erni%2C%20saya%20tertarik%20menjadi%20Mitra%20Lamonte.%20Bisa%20dibantu%20penjelasannya%3F">Hubungi Kami</CustomButton>
             </div>
         </section>
     );
