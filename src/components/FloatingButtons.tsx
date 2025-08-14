b7adbec89ca5e27c75c08b93b8cad162f841ff6f
lamonte-mi-web <datatimlamonte@gmail.com>
2025-08-14 11:05:43 +0700

    Floating Buttons Fixed

--- src/components/FloatingButtons.tsx
+++ src/components/FloatingButtons.tsx
@@ -1,9 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
-import CustomButton from "./CustomButton";
+import CTAButton from "./CTAButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
-import CTAButton from "./CTAButton";
 
 export default function FloatingButtons() {
   const [showScroll, setShowScroll] = useState(false);
@@ -23,27 +22,33 @@ export default function FloatingButtons() {
   return (
     <div
       id="scrollup"
-      className="fixed bottom-8 right-8sm:bottom-8 sm:right-8bottom-4 right-4 /* mobile */ flex flex-col items-end gap-3 z-50"
+      className={`
+    fixed bottom-4 z-50
+    sm:right-4 sm:flex sm:flex-col sm:items-end sm:gap-3
+    max-sm:left-0 max-sm:right-0 max-sm:flex max-sm:flex-row max-sm:justify-between max-sm:px-4
+  `}
     >
+      <div className="max-sm:self-start">
+        <CTAButton />
+      </div>
 
       <div
         className={`transition-all duration-500 ease-in-out transform ${showScroll
-          ? "translate-y-0 opacity-100" // move up when visible
-          : "translate-y-10 opacity-0 pointer-events-none" // hide + block clicks
+            ? "translate-y-0 opacity-100"
+            : "translate-y-10 opacity-0 pointer-events-none"
           }`}
       >
         <button
           id="scroll-top"
           onClick={scrollToTop}
-          className="w-12 h-12 sm:w-12 sm:h-12 w-10 h-10 /* mobile */ rounded-full bg-[#FF9000] text-white flex items-center justify-center shadow-lg hover:bg-[#229659] transition-colors duration-300
-  "
+          className="w-10 h-10 sm:w-12 sm:h-12 rounded-full bg-[#FF9000] text-white flex items-center justify-center shadow-lg hover:bg-[#229659] transition-colors duration-300"
         >
-          <FontAwesomeIcon icon={faAngleUp} className="text-white text-lg sm:text-xl" />
+          <FontAwesomeIcon
+            icon={faAngleUp}
+            className="text-white text-lg sm:text-xl"
+          />
         </button>
-
       </div>
-
-      <CTAButton />
     </div>
   );
 }
