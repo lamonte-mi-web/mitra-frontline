a88fc82477488c4bb79338a97b74cb383d34208a
lamonte-mi-web <datatimlamonte@gmail.com>
2025-08-18 10:34:50 +0700

    Hero Text Color Change & CTA button Changes

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -12,5 +12,5 @@ export default function CTAButton(props: Partial<CustomButtonProps>) {
         register(btnRef);
     }, [btnRef]);
 
-    return <CustomButton ref={btnRef} href="https://wa.me/628111209921?text=Halo%20Caroline%2C%20saya%20sudah%20lihat%20penawarannya%20dan%20ingin%20langsung%20daftar.%20Bisa%20dibantu%20sekarang%3F" {...props}>{props.children ?? "Gabung Sekarang"}</CustomButton>;
+    return <CustomButton ref={btnRef} href="https://wa.me/+628111089921?text=Halo%20Mila%2C%20saya%20sudah%20lihat%20penawarannya%20dan%20ingin%20langsung%20daftar.%20Bisa%20dibantu%20sekarang%3F" {...props}>{props.children ?? "Gabung Sekarang"}</CustomButton>;
 }
