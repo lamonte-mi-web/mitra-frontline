09744c9c936c9a694f99eafa7615e8946ffffc96
lamonte-mi-web <datatimlamonte@gmail.com>
2025-08-20 11:23:28 +0700

    Added cta-button class for cta buttons

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -12,5 +12,5 @@ export default function CTAButton(props: Partial<CustomButtonProps>) {
         register(btnRef);
     }, [btnRef]);
 
-    return <CustomButton id="cta-button" ref={btnRef} href="https://wa.me/+628111089921?text=Halo%20Mila%2C%20saya%20sudah%20lihat%20penawarannya%20dan%20ingin%20langsung%20daftar.%20Bisa%20dibantu%20sekarang%3F" {...props}>{props.children ?? "Gabung Sekarang"}</CustomButton>;
+    return <CustomButton className="cta-button" ref={btnRef} href="https://wa.me/+628111089921?text=Halo%20Mila%2C%20saya%20sudah%20lihat%20penawarannya%20dan%20ingin%20langsung%20daftar.%20Bisa%20dibantu%20sekarang%3F" {...props}>{props.children ?? "Gabung Sekarang"}</CustomButton>;
 }
